284cca95a640c77f180ebb25040f1e2d725f2ffb
Ahmedrizvi1 <108755847+Ahmedrizvi1@users.noreply.github.com>
2022-08-02 21:21:19 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello')
+@app.route("/")
 def home():
-    return 'Hello World'
+    return 'Hello!'
 
+
+@app.route("/wow")
+def wow():
+    return 'wow!'
 
 if __name__ == '__main__':
     app.run()
